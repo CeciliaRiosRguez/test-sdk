9a0dd1af296c31e33614c354a69dd8f8b101d901
Cecilia Rios <140440846+CeciliaRiosRguez@users.noreply.github.com>
2023-10-23 17:13:07 -0700

    test

---
+++ src/Scan.js
@@ -0,0 +1,23 @@
+import React, { useEffect } from "react";
+import ScanWrap from "scandoc-onboarding";
+import "scandoc-onboarding/dist/css/onboarding.css";
+
+function Scan() {
+    useEffect(() => {
+        const lib = new ScanWrap({
+            el: "videoCapturingEl",
+            domainApi: "https://fraudfighter.azure-api.net/test-router",
+            customerKey: "be608d0b-53c2-4c3a-8501-5e55ec17ceeb",
+            domainId: "5cf9a48f-0ae9-466d-915e-08dbd3f1ad4a",
+            publicKey: "pk_274eb098-b9df-43f5-84d2-d48d389bfc0e",
+            configApi: "https://api-dvsonline.idscan.net/",
+        });
+    }, []);
+    return (
+        <div className="App">
+            <div id="videoCapturingEl"></div>
+        </div>
+    );
+}
+
+export default Scan;
