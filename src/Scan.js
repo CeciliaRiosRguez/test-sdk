52212b8f38a096a6b782c5a1ca3e38664a350524
Cecilia Rios <140440846+CeciliaRiosRguez@users.noreply.github.com>
2023-10-24 18:10:58 -0700

    test

--- src/Scan.js
+++ src/Scan.js
@@ -10,7 +10,7 @@ function Scan() {
             customerKey: "be608d0b-53c2-4c3a-8501-5e55ec17ceeb",
             domainId: "5cf9a48f-0ae9-466d-915e-08dbd3f1ad4a",
             publicKey: "pk_274eb098-b9df-43f5-84d2-d48d389bfc0e",
-            configApi: "https://api-dvsonline.idscan.net/",
+            configApi: "https://fraudfighter.azure-api.net/test-router/",
         });
     }, []);
     return (
