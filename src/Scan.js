18d4a00b98e7eab399503e863bd1510066781760
Cecilia Rios <140440846+CeciliaRiosRguez@users.noreply.github.com>
2023-10-24 22:07:17 -0700

    test

--- src/Scan.js
+++ src/Scan.js
@@ -6,11 +6,11 @@ function Scan() {
     useEffect(() => {
         const lib = new ScanWrap({
             el: "videoCapturingEl",
-            domainApi: "https://fraudfighter.azure-api.net/test-router",
+            domainApi: "https://fraudfighter.azure-api.net/doc-router",
             customerKey: "be608d0b-53c2-4c3a-8501-5e55ec17ceeb",
             domainId: "5cf9a48f-0ae9-466d-915e-08dbd3f1ad4a",
             publicKey: "pk_274eb098-b9df-43f5-84d2-d48d389bfc0e",
-            configApi: "https://fraudfighter.azure-api.net/test-router/",
+            configApi: "https://fraudfighter.azure-api.net/doc-router/",
         });
     }, []);
     return (
